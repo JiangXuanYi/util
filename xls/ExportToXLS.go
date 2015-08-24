fecdde6e4c25b0c44412a2b0a493e913ad31c279
JiangFeng <you@example.com>
2015-08-24 11:11:52 +0800

    提交存储xls

---
+++ xls/ExportToXLS.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"encoding/csv"
+	"os"
+)
+
+func main() {
+
+	f, err := os.Create("1.xls")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	f.WriteString("\xEF\xBB\xBF")
+	w := csv.NewWriter(f)
+	w.Write([]string{"Number", "Name", "Age"})
+	w.Write([]string{"1", "Lucy", "23"})
+	w.Write([]string{"2", "Hanmeimei", "23"})
+	w.Write([]string{"3", "Lilei", "23"})
+	w.Write([]string{"4", "Lili", "23"})
+	w.Flush()
+
+}
